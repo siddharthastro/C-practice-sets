03a9ffa707e62ec050dc3a5734603408cb7398a8
siddharthastro <siddharth12108@gmail.com>
2026-06-30 01:42:37 +0000

    Add my missing files

---
+++ practice1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    //Defination
+    int num1;
+    int num2;
+    int sum;
+    //Getting inputs
+cout <<"Enter the first number: ";
+cin >> num1;
+cout <<"Enter the second number: ";
+cin >> num2;
+    //Calculating sum
+    sum = num1 + num2;
+    //Displaying sum
+    cout <<"The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+    return 0;
+}
