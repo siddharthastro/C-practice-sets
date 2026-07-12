d967c254e3f991b0daefada1ab5aa2b02b4c4d59
siddharthastro <siddharth12108@gmail.com>
2026-07-12 07:59:58 +0000

    New lines added

---
+++ practice2.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Welcome to the third practice session of C++ progrmamming Language" << endl;
+    cout << "This is the second line of the program" << endl;
+    cout << "This is the end of the program";
+    return 0;
+}
