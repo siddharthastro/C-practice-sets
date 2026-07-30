29ac7a7b45ccf2e7d6d7e7e5ffc497b6213723cb
siddharthastro <siddharth12108@gmail.com>
2026-07-30 10:42:26 +0000

    adding boolean type data

---
+++ practice6.cpp
@@ -0,0 +1,20 @@
+// getting used to the boolean data type
+#include <iostream>
+using namespace std;
+int main()
+{
+ bool a = 33;
+ bool b =  0;
+ bool c = -000000000;
+ bool d = 1;
+ bool e = -3766;
+ bool j = true;
+ bool f = false;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
+    cout << "e = " << e << endl;
+    cout << "j = " << j << endl;
+    cout << "f = " << f << endl;
+}
