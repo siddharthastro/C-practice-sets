d14fe7b36b3a2f0181c721a360852833aed61e09
siddharthastro <siddharth12108@gmail.com>
2026-07-30 10:14:20 +0000

    practice after some time

---
+++ practice5.cpp
@@ -0,0 +1,14 @@
+// Special characters 
+# include <iostream>
+using namespace std;
+int main()
+{
+cout << "Hello\n";
+cout << "Hi\tfriends"<<endl;
+cout << "Hello there  \bfriends"<<endl;
+cout << "I am here \rYou are a good person"<<endl;
+cout << "for a single quotation mark use hash \'"<<endl;
+cout << "for a double quatation mark use a hash \" "<<endl;
+cout << "for a backslash use a hash \\"<<endl;
+return 0;
+}
