072a90f84ad9092a4006ff6a8d3b03b070e69ac7
siddharthastro <siddharth12108@gmail.com>
2026-07-31 10:40:48 +0000

    Pyramid loop

---
+++ practice9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int alpha = 0;
+    int delta = 1;
+    cout << "Please give the height of the triangle:" <<endl;
+    cin >> alpha;
+    while (alpha >= delta)
+    {
+    cout << string(delta, 'X') << endl;
+    delta++;
+    }
+
+
+    
+
+}
