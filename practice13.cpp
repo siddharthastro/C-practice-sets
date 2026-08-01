2adc2ff672180f75275c503036c6dfd13e43e577
siddharthastro <siddharth12108@gmail.com>
2026-08-01 13:44:54 +0000

    Type change in implicit promotion

---
+++ practice13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace  std;
+int main(){
+    char c;
+    char d;
+    bool alpha;
+    alpha = c + d;
+    cout << c+d << endl;
+    cout << alpha << endl;
+    return 0;
+}
