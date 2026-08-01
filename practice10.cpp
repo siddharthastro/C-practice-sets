44166506c0803a2b4cf6256de806cd13fe5d4dde
siddharthastro <siddharth12108@gmail.com>
2026-08-01 04:57:11 +0000

    String library used

---
+++ practice10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string firstName;
+    string lastName;
+    string Initials;
+    string space = " ";
+    string dot = ".";
+    string fullname;
+    cout << "Please enter your first name: " << endl;
+    cin >> firstName;
+    cout << "Please enter your middle name: " << endl;
+    cin >> Initials;
+    cout << "Please enter your last name: " << endl;
+    cin >> lastName;
+    fullname = firstName + space + Initials + dot + space + lastName;
+    cout << "Your full name is: " << fullname << endl;
+}
