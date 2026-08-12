52333456c423b7e97c20075b62ae1b0b37a38921
siddharthastro <siddharth12108@gmail.com>
2026-08-12 15:24:54 +0000

    Grade counter using multiple selection

---
+++ practice20.cpp
@@ -0,0 +1,32 @@
+// grade counter 
+# include <iostream>
+using namespace std;
+int main()
+{
+    int score;
+    char grade;
+    cout << "Please enter your score" << endl;
+    cin >> score;
+    if (score >= 90)
+    {
+        grade = 'A';
+    }
+    else if (score >= 80)
+    {
+        grade = 'B';
+    }
+    else if (score >= 70)
+    {
+        grade = 'C';
+    }
+    else if (score >= 60)
+    {
+        grade = 'D';
+    }
+    else
+    {
+        grade = 'F';
+    }
+    cout << "Your grade is:" << grade << endl;
+return 0;
+}
